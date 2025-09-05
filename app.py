b4810275e2bea2f088ea6800158ea3f5d2b1689c
ShamSHa0987 <your_github_email@example.com>
2025-09-05 19:58:39 +0530

    app.py

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ private_subnet_1_cidr = "10.0.3.0/24"
 private_subnet_2_cidr = "10.0.4.0/24"
 private_subnet_3_cidr = "10.0.5.0/24"
 private_subnet_4_cidr = "10.0.6.0/24"
-private_subnet_5_cidr = "10.0.7.0/24"
-private_subnet_6_cidr = "10.0.8.0/24"i
+
+
 
 vpc_id            = module.vpc.vpc_id
  allowed_ssh_cidr = ["0.0.0.0/0"]   
